79db0b5b5deedfb05c003eb65c69cc4a55403abd
efemena-elvis <elvis@vesicash.com>
2025-01-29 04:34:26 +0100

    Updated abi base url

--- src/app/_utils/(configs)/app-env.ts
+++ src/app/_utils/(configs)/app-env.ts
@@ -1,7 +1,7 @@
-const API_VERSION = process.env.API_VERSION;
-const API_BASE_URL = process.env.API_BASE_URL;
-// const API_VERSION = "v1";
-// const API_BASE_URL = "http://localhost:4001/";
+// const API_VERSION = process.env.API_VERSION;
+// const API_BASE_URL = process.env.API_BASE_URL;
+const API_VERSION = "v1";
+const API_BASE_URL = "https://api.moospire.com/";
 
 const MOOSPIRE_AUTH_USER = "MOOSPIRE_AUTH_USER";
 
